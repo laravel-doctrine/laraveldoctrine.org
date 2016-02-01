93ad961226dc6efc8befb2f21945e3bd545cf06e
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-02-01 16:21:50 +0100

    Add fluent to navbar

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -2,6 +2,7 @@
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'extensions']) }}">Extensions</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'migrations']) }}">Migrations</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'acl']) }}">ACL</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'fluent']) }}">Fluent</a></li>
 
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Community</a>
