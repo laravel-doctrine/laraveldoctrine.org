d2f618378841c085a12ab9745011e91927a851bb
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-04-07 23:00:48 +0200

    Add community packages to main nav

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -7,7 +7,12 @@
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Community</a>
     <ul class="dropdown-menu" role="menu">
+        <h6 class="dropdown-header">Join us on</h6>
         <a class="dropdown-item" target="_blank" href="https://github.com/laravel-doctrine">GitHub</a>
         <a class="dropdown-item" target="_blank" href="http://slack.laraveldoctrine.org">Slack</a>
+        <div class="dropdown-divider"></div>
+        <h6 class="dropdown-header">Community packages</h6>
+        <a class="dropdown-item" target="_blank" href="https://github.com/dave-redfern/laravel-doctrine-behaviours">Laravel Doctrine Behaviours</a>
+        <a class="dropdown-item" target="_blank" href="https://github.com/bharley/laraveldoctrine-hashids">Hashids</a>
     </ul>
 </li>
