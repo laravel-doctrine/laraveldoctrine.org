47475b02e83d49d4187a1a84901f62c1c6137978
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-09-03 11:38:26 +0200

    Add scout to main nav

--- resources/views/partials/main-nav.blade.php
+++ resources/views/partials/main-nav.blade.php
@@ -3,6 +3,7 @@
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'migrations']) }}">Migrations</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'acl']) }}">ACL</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'fluent']) }}">Fluent</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ route('docs.index', ['version' => $version, 'package' => 'scout']) }}">Scout</a></li>
 
 <li class="nav-item dropdown">
     <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Community</a>
@@ -14,5 +15,6 @@
         <h6 class="dropdown-header">Community packages</h6>
         <a class="dropdown-item" target="_blank" href="https://github.com/dave-redfern/laravel-doctrine-behaviours">Laravel Doctrine Behaviours</a>
         <a class="dropdown-item" target="_blank" href="https://github.com/bharley/laraveldoctrine-hashids">Hashids</a>
+        <a class="dropdown-item" target="_blank" href="https://github.com/maxbrokman/SafeQueue">SafeQueue</a>
     </ul>
 </li>
