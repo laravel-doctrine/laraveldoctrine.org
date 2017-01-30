822b2d8792bd2b3f9e3e69350de651494765c4ba
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2017-01-30 13:17:22 +0100

    Add 1.3 as version

--- app/Services/GithubDocsRepository.php
+++ app/Services/GithubDocsRepository.php
@@ -14,7 +14,7 @@ class GithubDocsRepository implements DocsRepository
      */
     public function getLatestVersion()
     {
-        return '1.2';
+        return '1.3';
     }
 
     /**
@@ -23,6 +23,7 @@ class GithubDocsRepository implements DocsRepository
     public function getVersions()
     {
         return [
+            '1.3',
             '1.2',
             '1.1',
             '1.0'
